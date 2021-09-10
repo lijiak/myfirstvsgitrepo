b20532d0624e6bf69bbc7bcddd274101a90c4369
lijiakanggit <gaanalyticcpi@gmail.com>
2021-09-10 10:36:07 +0800

    LocalCommit2:AllChangesMade

---
+++ DashboardTry/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DashboardTry.Startup))]
+namespace DashboardTry
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
